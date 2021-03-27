c12e2266fbad940bb50f8302372fc37dda4c9c78
Wendy Chen <jaewchen@gmail.com>
2021-03-27 13:26:26 -0500

    Finished python day5.

--- python/day5/main.py
+++ python/day5/main.py
@@ -1,8 +1,8 @@
 import utils
 
 def main():
-  content = utils.readFile('../../inputs/day5-sample.txt')
-  print("Highest seat ID is " + str(utils.findElements(content)) + ".")
+  content = utils.readFile('../../inputs/day5.txt')
+  print("Highest seat ID is " + str(utils.findElements(content, 128, 8)) + ".")
 
 if __name__ == "__main__":
   main()
--- python/day5/utils.py
+++ python/day5/utils.py
@@ -5,6 +5,19 @@ def readFile(name):
     content = [line.rstrip() for line in f.readlines() if re.compile(r'[FB]{7}[LR]{3}').match(line) is not None]
   return content
 
-def findElements(binary):
-  print(binary)
-  return 0
+def binarySplit(str, minV, maxV):
+  if minV == maxV or maxV == 0 or len(str) == 0:
+    return maxV
+  
+  isLower = str[0] in ["F", "L"]
+  halved = (minV + maxV) // 2
+  return binarySplit(str[1:], minV if isLower else halved + 1, halved if isLower else maxV)
+
+def split(str, total):
+  return binarySplit(str, 0, total - 1)
+
+def findElements(binary, rows, columns):
+  seatIds = [split(b[:7], rows) * columns + split(b[7:], columns) for b in binary]
+  emptySeatIds = set(range(0, max(seatIds) + 1)) - set(seatIds)
+  nonConsecutiveEmptySeatIds = [s for s in emptySeatIds if s - 1 not in emptySeatIds and s + 1 not in emptySeatIds]
+  return nonConsecutiveEmptySeatIds[0] if len(nonConsecutiveEmptySeatIds) > 0 else -1
